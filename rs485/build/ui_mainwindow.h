b3336aef880f97c62d50675e2f287188fe21f6c4
MS_KIM <46109587+msun98@users.noreply.github.com>
2023-02-24 09:35:55 +0900

    Add files via upload

---
+++ rs485/build/ui_mainwindow.h
@@ -0,0 +1,73 @@
+/********************************************************************************
+** Form generated from reading UI file 'mainwindow.ui'
+**
+** Created by: Qt User Interface Compiler version 5.12.8
+**
+** WARNING! All changes made in this file will be lost when recompiling UI file!
+********************************************************************************/
+
+#ifndef UI_MAINWINDOW_H
+#define UI_MAINWINDOW_H
+
+#include <QtCore/QVariant>
+#include <QtWidgets/QApplication>
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
+#include <QtWidgets/QStatusBar>
+#include <QtWidgets/QWidget>
+
+QT_BEGIN_NAMESPACE
+
+class Ui_MainWindow
+{
+public:
+    QWidget *centralwidget;
+    QPushButton *btnConnect;
+    QPushButton *btnDisConnect;
+    QMenuBar *menubar;
+    QStatusBar *statusbar;
+
+    void setupUi(QMainWindow *MainWindow)
+    {
+        if (MainWindow->objectName().isEmpty())
+            MainWindow->setObjectName(QString::fromUtf8("MainWindow"));
+        MainWindow->resize(800, 600);
+        centralwidget = new QWidget(MainWindow);
+        centralwidget->setObjectName(QString::fromUtf8("centralwidget"));
+        btnConnect = new QPushButton(centralwidget);
+        btnConnect->setObjectName(QString::fromUtf8("btnConnect"));
+        btnConnect->setGeometry(QRect(300, 200, 89, 25));
+        btnDisConnect = new QPushButton(centralwidget);
+        btnDisConnect->setObjectName(QString::fromUtf8("btnDisConnect"));
+        btnDisConnect->setGeometry(QRect(400, 200, 89, 25));
+        MainWindow->setCentralWidget(centralwidget);
+        menubar = new QMenuBar(MainWindow);
+        menubar->setObjectName(QString::fromUtf8("menubar"));
+        menubar->setGeometry(QRect(0, 0, 800, 22));
+        MainWindow->setMenuBar(menubar);
+        statusbar = new QStatusBar(MainWindow);
+        statusbar->setObjectName(QString::fromUtf8("statusbar"));
+        MainWindow->setStatusBar(statusbar);
+
+        retranslateUi(MainWindow);
+
+        QMetaObject::connectSlotsByName(MainWindow);
+    } // setupUi
+
+    void retranslateUi(QMainWindow *MainWindow)
+    {
+        MainWindow->setWindowTitle(QApplication::translate("MainWindow", "MainWindow", nullptr));
+        btnConnect->setText(QApplication::translate("MainWindow", "connect", nullptr));
+        btnDisConnect->setText(QApplication::translate("MainWindow", "disconnect", nullptr));
+    } // retranslateUi
+
+};
+
+namespace Ui {
+    class MainWindow: public Ui_MainWindow {};
+} // namespace Ui
+
+QT_END_NAMESPACE
+
+#endif // UI_MAINWINDOW_H
---
+++ rs485/mainwindow.h
@@ -0,0 +1,39 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QHostAddress>
+#include <iostream>
+#include <QSerialPort>
+#include <QSerialPortInfo>
+#include <QTimer>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+    void onReadyCmdRead();
+
+private slots:
+    void readData();
+
+    void handleError(QSerialPort::SerialPortError error);
+
+    void on_btnConnect_clicked();
+
+    void on_btnDisConnect_clicked();
+
+
+private:
+    Ui::MainWindow *ui;
+
+    QSerialPort* m_serialPort = nullptr;
+};
+#endif // MAINWINDOW_H
